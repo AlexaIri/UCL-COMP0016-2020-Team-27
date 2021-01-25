804aa4edd67e80ffe195d955309f6982033157bd
Alexandra Irimia <alexandrairimia27@yahoo.com>
2021-01-25 18:41:41 +0200

    enlisting all the feedbacks per reviewer

--- CFP_Portal/urls.py
+++ CFP_Portal/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name = 'blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('about/', views.about, name = 'blog-about'),
     path('displayOrganisations', views.index, name= 'index'),
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
--- CFP_Portal/views.py
+++ CFP_Portal/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.models import User
 from .models import Organisation, Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -72,3 +73,13 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
         #return super().test_func()
+
+class UserPostListView(ListView):
+    model = Post
+    template_name ='CFP_Portal/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author = user).order_by('-date_posted')
